c369f871ea956c7d6520ac58b2058e98a358816f
Marek <mpietrewicz@gmail.com>
2017-04-04 18:11:34 +0200

    Realizacja zadania Spiewacy. Brak skończonych metod spiewaj i najglosniej.

---
+++ src/main/java/Spiewacy/Main.java
@@ -0,0 +1,36 @@
+package Spiewacy;
+
+/**
+ * Created by Marek on 2017-03-31.
+ */
+public class Main {
+    public static void main(String[] args)
+    {
+        Spiewak s1 = new Spiewak("Carrey"){
+            @Override
+            String spiewaj() {
+                return "oooooooooooo";
+            }
+        };
+
+        Spiewak s2 = new Spiewak("Houston"){
+            String spiewaj() {
+                return "a4iBBiii";
+            }
+        };
+
+        Spiewak s3 = new Spiewak("Madonna"){
+            String spiewaj() {
+                return "aAa";
+            }
+        };
+
+        Spiewak sp[] = {s1, s2, s3};
+
+        for (Spiewak s : sp)
+            System.out.println(s);
+
+        System.out.println("\n" + Spiewak.najglosniej(sp));
+    }
+}
+
--- src/main/java/Spiewacy/Spiewak.java
+++ src/main/java/Spiewacy/Spiewak.java
@@ -3,5 +3,28 @@ package Spiewacy;
 /**
  * Created by Marek on 2017-03-31.
  */
-public class Spiewak {
+public abstract class Spiewak {
+    protected String nazwisko;
+    protected static int aktualnyNumerStartowy;
+    protected int numerStartowy;
+
+    public Spiewak(String nazwisko) {
+        this.nazwisko = nazwisko;
+        this.aktualnyNumerStartowy++;
+        this.numerStartowy = aktualnyNumerStartowy;
+    }
+
+    abstract String spiewaj();
+
+    @Override
+    public String toString() {
+        return "(" + numerStartowy + ") " + nazwisko;
+    }
+
+    static public Spiewak najglosniej(Spiewak[] spiewacy) {
+        return spiewacy[0];
+    }
+
+
+
 }
